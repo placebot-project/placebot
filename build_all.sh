2b4811104b72ea6a4662f47cf78d4c8ea348d8c9
thatretrodev <thatretrodev@outlook.com>
2022-04-08 19:49:48 -0400

    Update rewrite

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd packages/core; npm i && npm run build; cd ../..
-cd packages/cli; npm i && npm run build; cd ../..
+
+cd packages/cli
+
+npm i && npm run build
+
+npx pkg -t node16-linux-x64 -o dist/placebot-linux-amd64 dist/index.js
+npx pkg -t node16-windows-x64 -o dist/placebot-windows-amd64 dist/index.js
+
+cd ../..
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,16 @@
 const protocol = require('@placebot/core');
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync(process.argv[2], "utf8"));
+
+if (process.argv.length == 4) {
+	require('dotenv').config({ path: process.argv[3] });
+}
+else {
+	require('dotenv').config();
+}
 
 if (require.main == module) {
+	console.log(config);
 	console.log(protocol.encodePixel(0, 0, [255, 255, 255]));
 }
