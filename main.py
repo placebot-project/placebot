9da9414afc131e78c733c0930fc8f197856cac1d
thatretrodev <thatretrodev@outlook.com>
2022-04-09 14:00:53 -0400

    Add Random Mode

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from headers import *
 from canvas import *
 from PIL import Image
 import asyncio
+import random
 import time
 import sys
 
@@ -11,25 +12,39 @@ im = Image.open(sys.argv[1]).convert("RGB")
 pixels = im.load()
 width, height = im.size
 
-offset = ((1024 // 2) - (width // 2), (1024 // 2) - (height // 2))
+#offset = (387, 381)
+#offset = (620, 508)
+offset = (55, 68)
+#offset = (114, 5)
+# offset = ((1024 // 2) - (width // 2), (1024 // 2) - (height // 2))
 
 async def bot(uri):
 	async with connect(uri, extra_headers=headers) as websocket:
 		canvas_im = get_canvas()
 		canvas_pixels = canvas_im.load()
 
-		y_startpos = int(sys.argv[3])
+		y_startpos = 0#int(sys.argv[3])
 
 		for _y in range(height - y_startpos):
 			for x in range(width):
-				y = _y + y_startpos
-				color = pixels[x, y]
+				#y = _y + y_startpos
 
 				#print(canvas_pixels[offset[0] + x, offset[1] + y])
 				#print(pixels[x, y])
 
+				x = 0
+				y = 0
+
+				while canvas_pixels[offset[0] + x, offset[1] + y] == pixels[x, y]:
+					x = random.randint(0, width - 1)
+					y = random.randint(0, height - 1)
+					print(x, y)
+				
+				color = pixels[x, y]
+
 				if canvas_pixels[offset[0] + x, offset[1] + y] != pixels[x, y]:
 					#print(x, y)
+					print("Updating " + str(offset[0] + x) + ", " + str(offset[1] + y))
 					await websocket.send(encode_pixel(offset[0] + x, offset[1] + y, (color[0], color[1], color[2])))
 
 					packet_data = await websocket.recv()
@@ -37,9 +52,17 @@ async def bot(uri):
 					if len(packet_data) == 11:
 						decoded_data = decode_pixel(packet_data)
 
-						print("Updating " + str(decoded_data["x"]) + ", " + str(decoded_data["x"]))
+						#print("Updating " + str(decoded_data["x"]) + ", " + str(decoded_data["x"]))
 						canvas_pixels[decoded_data["x"], decoded_data["y"]] = decoded_data["color"]
 
-					time.sleep(0.3)
+					await asyncio.sleep(0.125)
 
-asyncio.run(bot(sys.argv[2]))##"ws://localhost:3000/ws"))
+asyncio.run(bot("wss://pl.g7kk.com/ws"))
+
+while True:
+	try:
+		asyncio.run(bot("wss://pl.g7kk.com/ws"))##"ws://localhost:3000/ws"))
+		break
+	except:
+		pass
+	time.sleep(0.5)
